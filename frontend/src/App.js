7c1b653337b2d5ae83be075fdc1ffce54b5f26e9
kaushal612 <kbhalaiya@gmail.com>
2022-05-31 15:27:14 +0530

    added Confirm Order section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,6 +26,7 @@ import ForgotPassword from './component/User/ForgotPassword.js';
 import ResetPassword from './component/User/ResetPassword.js';
 import Cart from './component/Cart/Cart.js';
 import Shipping from './component/Cart/Shipping.js';
+import ConfirmOrder from './component/Cart/ConfirmOrder.js';
 
 
 
@@ -80,6 +81,8 @@ function App() {
       <ProtectedRoute exact path="/password/update" component={UpdatePassword} />
 
       <ProtectedRoute exact path="/shipping" component={Shipping} />
+      
+      <ProtectedRoute exact path="/order/confirm" component={ConfirmOrder} />
 
 
 
