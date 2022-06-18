c22e65c965b93fdd31ea9ec57adf569f0f58d56e
kaushal612 <kbhalaiya@gmail.com>
2022-06-19 01:27:37 +0530

    Added Admin dashboard

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -181,7 +181,7 @@ export const resetPassword = (token, passwords) => async (dispatch) => {
     }
 }
 
-    
+
 
 
 
@@ -203,4 +203,67 @@ export const updatePassword = (userData) => async (dispatch) => {
         });
     }
 }
+
+
+// get All Users --Admin
+export const getAllUsers = () => async (dispatch) => {
+    try {
+        dispatch({ type: ALL_USERS_REQUEST });
+        const { data } = await axios.get(`/api/v1/admin/users`);
+
+        dispatch({ type: ALL_USERS_SUCCESS, payload: data.users });
+    } catch (error) {
+        dispatch({ type: ALL_USERS_FAIL, payload: error.response.data.message });
+    }
+};
+
+// get  User Details --Admin
+export const getUserDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: USER_DETAILS_REQUEST });
+        const { data } = await axios.get(`/api/v1/admin/user/${id}`);
+
+        dispatch({ type: USER_DETAILS_SUCCESS, payload: data.user });
+    } catch (error) {
+        dispatch({ type: USER_DETAILS_FAIL, payload: error.response.data.message });
+    }
+};
+
+// Update User --Admin
+export const updateUser = (id, userData) => async (dispatch) => {
+    try {
+        dispatch({ type: UPDATE_USER_REQUEST });
+
+        const config = { headers: { "Content-Type": "application/json" } };
+
+        const { data } = await axios.put(
+            `/api/v1/admin/user/${id}`,
+            userData,
+            config
+        );
+
+        dispatch({ type: UPDATE_USER_SUCCESS, payload: data.success });
+    } catch (error) {
+        dispatch({
+            type: UPDATE_USER_FAIL,
+            payload: error.response.data.message,
+        });
+    }
+};
+
+// Delete User --Admin
+export const deleteUser = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: DELETE_USER_REQUEST });
+
+        const { data } = await axios.delete(`/api/v1/admin/user/${id}`);
+
+        dispatch({ type: DELETE_USER_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({
+            type: DELETE_USER_FAIL,
+            payload: error.response.data.message,
+        });
+    }
+};
 
